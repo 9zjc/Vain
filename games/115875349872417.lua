25fa8ca30df437a4a05100908a41d77e747cb1a5
9zjc <jackyy2123@gmail.com>
2026-09-13 16:07:02 -0400

    Harden target angle math in 115875349872417.lua

--- games/115875349872417.lua
+++ games/115875349872417.lua
@@ -1087,7 +1087,9 @@ run(function()
 	
 		if ent then
 			local delta = (ent.RootPart.Position - selfpos)
-			local angle = math.acos(localfacing:Dot((delta * Vector3.new(1, 0, 1)).Unit))
+			local flatDelta = delta * Vector3.new(1, 0, 1)
+			if flatDelta.Magnitude < 0.001 or localfacing.Magnitude < 0.001 then return end
+			local angle = math.acos(math.clamp(localfacing.Unit:Dot(flatDelta.Unit), -1, 1))
 			if angle > (math.rad(AngleSlider.Value) / 2) then
 				return
 			end
